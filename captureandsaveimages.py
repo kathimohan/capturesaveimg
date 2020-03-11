78adab16109a9ee589f5cacbd6ff964c793e11f7
kathimohan <36533398+kathimohan@users.noreply.github.com>
2020-03-11 10:29:07 +0530

    Add files via upload

---
+++ captureandsaveimages.py
@@ -0,0 +1,9 @@
+import cv2
+videoCaptureObject = cv2.VideoCapture(0)
+result = True
+while(result):
+    ret,frame = videoCaptureObject.read()
+    cv2.imwrite("NewPicture.jpg",frame)
+    result = False
+videoCaptureObject.release()
+cv2.destroyAllWindows()
